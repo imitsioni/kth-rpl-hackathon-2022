2f81561663c13935df6475ba743cdc535344f939
igmitsio <35767563+imitsioni@users.noreply.github.com>
2022-03-16 13:18:39 +0100

    Create example.py for the tutorial

---
+++ tutorial/example.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sum_of_squares(a: int, b: int) -> float:
+    """This function calculates the difference of squares of two integers.
+
+    Args:
+        a (int): Value to be squared and the minuend of the subtraction
+        b (int): Value to be squared and the subtrahend of the subtraction
+
+    Returns:
+        float: The difference of squares
+    """
+    # Complete the following line with the difference of squares
+    diff = 0
+
+    return diff
+
+
+a = [2, 3, 4, 5, 6]
+b = [7, 9, 5, 13, 11]
+diffs = []
+# Calculate the differences of squares of the integers in lists a and b and
+#  plot them
+
+plt.figure()
+plt.plot(diffs)
+plt.show()
