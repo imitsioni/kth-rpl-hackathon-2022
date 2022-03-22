8683d2314b120c7e500a9eecf5ac4ab4f71a4248
carol <iyuner1@gmail.com>
2022-03-23 00:26:51 +0100

    WIP: add ros interface

--- src/geometry_utils.py
+++ src/geometry_utils.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import copy
-
+import rospy
+from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+from ctypes import *
 
 def read_pointcloud(path: str, view: bool = False) -> o3d.geometry.PointCloud:
     '''
@@ -196,3 +200,84 @@ def get_width_length_height(pcd, max_plane_idx=2, view=False) -> [np.array, np.a
         test2_pcd.colors = o3d.utility.Vector3dVector(color2point)
         o3d.visualization.draw_geometries([box_points] + [test_pcd] + [test2_pcd])
     return np.array([width, length, height]), center
+
+
+### below refer to https://github.com/kaku756/icp_calib/blob/b6f9af98019990f6111836dcbad4e97e5b69f692/scripts/lib_cloud_conversion_between_Open3D_and_ROS.py
+
+
+# The data structure of each point in ros PointCloud2: 16 bits = x + y + z + rgb
+FIELDS_XYZ = [
+    PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+]
+FIELDS_XYZRGB = FIELDS_XYZ + \
+    [PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
+
+# Bit operations
+BIT_MOVE_16 = 2**16
+BIT_MOVE_8 = 2**8
+convert_rgbUint32_to_tuple = lambda rgb_uint32: (
+    (rgb_uint32 & 0x00ff0000)>>16, (rgb_uint32 & 0x0000ff00)>>8, (rgb_uint32 & 0x000000ff)
+)
+convert_rgbFloat_to_tuple = lambda rgb_float: convert_rgbUint32_to_tuple(
+    int(cast(pointer(c_float(rgb_float)), POINTER(c_uint32)).contents.value)
+)
+
+# Convert the datatype of point cloud from Open3D to ROS PointCloud2 (XYZRGB only)
+def convertCloudFromOpen3dToRos(open3d_cloud: o3d.geometry.OrientedBoundingBox, frame_id="odom") -> PointCloud2:
+    # Set "header"
+    header = Header()
+    header.stamp = rospy.Time.now()
+    header.frame_id = frame_id
+
+    # Set "fields" and "cloud_data"
+    points=np.asarray(open3d_cloud.points)
+    if not open3d_cloud.colors: # XYZ only
+        fields=FIELDS_XYZ
+        cloud_data=points
+    else: # XYZ + RGB
+        fields=FIELDS_XYZRGB
+        # -- Change rgb color from "three float" to "one 24-byte int"
+        # 0x00FFFFFF is white, 0x00000000 is black.
+        colors = np.floor(np.asarray(open3d_cloud.colors)*255) # nx3 matrix
+        colors = colors[:,0] * BIT_MOVE_16 +colors[:,1] * BIT_MOVE_8 + colors[:,2]  
+        cloud_data=np.c_[points, colors]
+    
+    # create ros_cloud
+    return pc2.create_cloud(header, fields, cloud_data)
+
+def convertCloudFromRosToOpen3d(ros_cloud: PointCloud2) -> o3d.geometry.PointCloud:
+    # Get cloud data from ros_cloud
+    field_names=[field.name for field in ros_cloud.fields]
+    cloud_data = list(pc2.read_points(ros_cloud, skip_nans=True, field_names = field_names))
+
+    # Check empty
+    open3d_cloud = o3d.geometry.PointCloud()
+    if len(cloud_data)==0:
+        print("Converting an empty cloud")
+        return None
+
+    # Set open3d_cloud
+    if "rgb" in field_names:
+        IDX_RGB_IN_FIELD=3 # x, y, z, rgb
+        
+        # Get xyz
+        xyz = [(x,y,z) for x,y,z,rgb in cloud_data ] # (why cannot put this line below rgb?)
+
+        # Get rgb
+        # Check whether int or float
+        if type(cloud_data[0][IDX_RGB_IN_FIELD])==float: # if float (from pcl::toROSMsg)
+            rgb = [convert_rgbFloat_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
+        else:
+            rgb = [convert_rgbUint32_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
+
+        # combine
+        open3d_cloud.points = o3d.utility.Vector3dVector(np.array(xyz))
+        open3d_cloud.colors = o3d.utility.Vector3dVector(np.array(rgb)/255.0)
+    else:
+        xyz = [(x,y,z) for x,y,z in cloud_data ] # get xyz
+        open3d_cloud.points = o3d.utility.Vector3dVector(np.array(xyz))
+
+    # return
+    return open3d_cloud
