9d4314aef4f2a8f6d3c95773f3e5641fc88e0547
Celiali <cecilialikth@gmail.com>
2022-03-25 02:14:55 +0100

    add median filter (need to check)

--- src/get_box_geo.py
+++ src/get_box_geo.py
@@ -15,7 +15,7 @@ class box_geometry:
         rospy.init_node('box_geometry')
         self.rate = rospy.Rate(0.5)  # publish message at 0.5 Hz, still not good enough
         # self.sub = rospy.Subscriber("/camera/depth/color/points", PointCloud2, self.callback, queue_size=1)
-        
+
         # TODO
         self.sub = rospy.Subscriber("/camera_kinect/depth/points", PointCloud2, self.callback, queue_size=1)
         self.geometry_pub = rospy.Publisher("bbox/geometry", OrientedBoundingBox, queue_size=1)
@@ -37,7 +37,12 @@ class box_geometry:
         self.pub_corner1_world = rospy.Publisher("bbox_world/corner1", PointStamped, queue_size=1)
         self.pub_corner2_world = rospy.Publisher("bbox_world/corner2", PointStamped, queue_size=1)
         self.pub_corner3_world = rospy.Publisher("bbox_world/corner3", PointStamped, queue_size=1)
-        
+
+        # buffer
+        self.buffer_surface = []
+        self.buffer_box_geometry = []
+        self.buffer_center = []
+
         # rospy.spin()
         # self.listener = tf.TransformListener()
         self.rate.sleep()  
@@ -69,55 +74,74 @@ class box_geometry:
         o3d_pcl = convertCloudFromRosToOpen3d(data)
         bbox = OrientedBoundingBox()
         geometry, center, top_surface_corners = get_width_length_height(o3d_pcl, max_plane_idx=2, view=False)
-        bbox.pose.position.x, bbox.pose.position.y, bbox.pose.position.z = center[0], center[1], center[2]
-        bbox.extents.x, bbox.extents.y, bbox.extents.z = geometry[0]+0.01, geometry[1]+0.01, geometry[2]
-        
-        # cor0 = Point(top_surface_corners[0,0], top_surface_corners[0,1], top_surface_corners[0,2])
-        # cor1 = Point(top_surface_corners[1,0], top_surface_corners[1,1], top_surface_corners[1,2])
-        # cor2 = Point(top_surface_corners[2,0], top_surface_corners[2,1], top_surface_corners[2,2])
-        # cor3 = Point(top_surface_corners[3,0], top_surface_corners[3,1], top_surface_corners[3,2])
-        
-        cor_world = np.array([self.transform_to_world(top_surface_corners[x]) for x in range(top_surface_corners.shape[0])])
-        # y_increasing_ind = np.argsort(cor_world, axis=0) # increasing order
-        y_increasing = np.argsort(cor_world[:,1])
-        cor_world = cor_world[y_increasing]
-        threshold = 0.01
-        if cor_world[1,1] - cor_world[0,1]> threshold:
-            cor0_world = cor_world[0]
-            cor3_world = cor_world[3]
-            cor1_world = cor_world[2] if (cor_world[1,0] < cor_world[2,0]) else cor_world[1]
-            cor2_world = cor_world[2] if (cor_world[1,0] > cor_world[2,0]) else cor_world[1]
-        else:
-            cor2_world = cor_world[0] if (cor_world[0,0] < cor_world[1,0]) else cor_world[1]
-            cor0_world = cor_world[0] if (cor_world[0,0] > cor_world[1,0]) else cor_world[1]
-            cor1_world = cor_world[3] if (cor_world[2,0] < cor_world[3,0]) else cor_world[2]
-            cor3_world = cor_world[3] if (cor_world[2,0] > cor_world[3,0]) else cor_world[2]
 
+        self.buffer_box_geometry.append(geometry)
+        self.buffer_center.append(center)
+        self.buffer_surface.append(top_surface_corners)
+        assert len(self.buffer_box_geometry) == len(self.buffer_center) ==  len(self.buffer_surface)
+        if len(self.buffer_box_geometry) == 20: # TODO: set the seq_length
+            buffer_box_geometry_numpy = np.array(self.buffer_box_geometry)
+            buffer_center_numpy = np.array(self.buffer_center)
+            buffer_surface_numpy = np.array(self.buffer_surface)
+            # TODO: need to double check the median results
+            geometry = np.median(buffer_box_geometry_numpy, axis=0)
+            center = np.median(buffer_center_numpy, axis=0)
+            top_surface_corners = np.median(buffer_surface_numpy, axis=0)
 
-        
-        world_frame = copy.copy(data.header)
-        world_frame.frame_id='world'
-        stamp = data.header.stamp
+            # bbox.pose.position.x, bbox.pose.position.y, bbox.pose.position.z = center[0], center[1], center[2]
+            center_world = self.transform_to_world(center) # TODO: transform the box center to the world frame
+            bbox.pose.position.x, bbox.pose.position.y, bbox.pose.position.z = center_world[0], center_world[1], center_world[2]
+            bbox.extents.x, bbox.extents.y, bbox.extents.z = geometry[0]+0.01, geometry[1]+0.01, geometry[2]
 
-        cor0_world = PointStamped(world_frame, self.conver_to_point(cor0_world))
-        cor1_world = PointStamped(world_frame, self.conver_to_point(cor1_world))
-        cor2_world = PointStamped(world_frame, self.conver_to_point(cor2_world))
-        cor3_world = PointStamped(world_frame, self.conver_to_point(cor3_world))
+            # cor0 = Point(top_surface_corners[0,0], top_surface_corners[0,1], top_surface_corners[0,2])
+            # cor1 = Point(top_surface_corners[1,0], top_surface_corners[1,1], top_surface_corners[1,2])
+            # cor2 = Point(top_surface_corners[2,0], top_surface_corners[2,1], top_surface_corners[2,2])
+            # cor3 = Point(top_surface_corners[3,0], top_surface_corners[3,1], top_surface_corners[3,2])
 
-        self.geometry_pub.publish(bbox)
-        # self.pub_corner0.publish(PointStamped(data.header, cor0))
-        # self.pub_corner1.publish(PointStamped(data.header, cor1))
-        # self.pub_corner2.publish(PointStamped(data.header, cor2))
-        # self.pub_corner3.publish(PointStamped(data.header, cor3))
+            cor_world = np.array([self.transform_to_world(top_surface_corners[x]) for x in range(top_surface_corners.shape[0])])
+            # y_increasing_ind = np.argsort(cor_world, axis=0) # increasing order
+            y_increasing = np.argsort(cor_world[:,1])
+            cor_world = cor_world[y_increasing]
+            cor_world[:,2] = bbox.pose.position.z # TODO: replacing the z axis as the z axis of the box center(in world frame)
+            threshold = 0.01
+            if cor_world[1,1] - cor_world[0,1]> threshold:
+                cor0_world = cor_world[0]
+                cor3_world = cor_world[3]
+                cor1_world = cor_world[2] if (cor_world[1,0] < cor_world[2,0]) else cor_world[1]
+                cor2_world = cor_world[2] if (cor_world[1,0] > cor_world[2,0]) else cor_world[1]
+            else:
+                cor2_world = cor_world[0] if (cor_world[0,0] < cor_world[1,0]) else cor_world[1]
+                cor0_world = cor_world[0] if (cor_world[0,0] > cor_world[1,0]) else cor_world[1]
+                cor1_world = cor_world[3] if (cor_world[2,0] < cor_world[3,0]) else cor_world[2]
+                cor3_world = cor_world[3] if (cor_world[2,0] > cor_world[3,0]) else cor_world[2]
 
-        self.pub_corner0_world.publish(cor0_world)
-        self.pub_corner1_world.publish(cor1_world)
-        self.pub_corner2_world.publish(cor2_world)
-        self.pub_corner3_world.publish(cor3_world)
+            world_frame = copy.copy(data.header)
+            world_frame.frame_id='world'
+            stamp = data.header.stamp
 
-        rospy.loginfo(geometry)
-        # rospy.loginfo(top_surface_corners)
+            cor0_world = PointStamped(world_frame, self.conver_to_point(cor0_world))
+            cor1_world = PointStamped(world_frame, self.conver_to_point(cor1_world))
+            cor2_world = PointStamped(world_frame, self.conver_to_point(cor2_world))
+            cor3_world = PointStamped(world_frame, self.conver_to_point(cor3_world))
 
+            self.geometry_pub.publish(bbox)
+            # self.pub_corner0.publish(PointStamped(data.header, cor0))
+            # self.pub_corner1.publish(PointStamped(data.header, cor1))
+            # self.pub_corner2.publish(PointStamped(data.header, cor2))
+            # self.pub_corner3.publish(PointStamped(data.header, cor3))
+
+            self.pub_corner0_world.publish(cor0_world)
+            self.pub_corner1_world.publish(cor1_world)
+            self.pub_corner2_world.publish(cor2_world)
+            self.pub_corner3_world.publish(cor3_world)
+
+            rospy.loginfo(geometry)
+            # rospy.loginfo(top_surface_corners)
+
+            # buffer # TODO: every time publish, clean the buffer
+            self.buffer_surface = []
+            self.buffer_box_geometry = []
+            self.buffer_center = []
 
 if __name__ == '__main__':
     box_geometry()
