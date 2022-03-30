40c0d4b34349388690ba2ff8f2460d1e4feffdf5
daniel <dlmarta@kth.se>
2022-03-30 19:12:46 +0200

    re-formating main branch

--- src/state_machine/test_folding.py
+++ src/state_machine/test_folding.py
@@ -17,279 +17,306 @@ import rospy
 ## Planning imports
 
 
-
 # Defines start node to get all info
 class Start(smach.State):
 
-    def __init__(self,
-                 outcomes=['sucess'],
-                 input_keys=['foo_counter_in'],
-                 output_keys=['foo_counter_out']):
-        smach.State.__init__(self,
-                             outcomes=outcomes,
-                             input_keys=['foo_counter_in'],
-                             output_keys=['foo_counter_out'])
+	def __init__(self,
+	             outcomes=['sucess'],
+	             input_keys=['foo_counter_in'],
+	             output_keys=['foo_counter_out']):
+		smach.State.__init__(self,
+		                     outcomes=outcomes,
+		                     input_keys=['foo_counter_in'],
+		                     output_keys=['foo_counter_out'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state Identify Corner')
-        time.sleep(2)
-        return 'sucess'
+	def execute(self, userdata):
+		rospy.loginfo('Executing state Identify Corner')
+		time.sleep(2)
+		return 'sucess'
 
 
 class IdentifyCorner(smach.State):
 
-    def __init__(self,
-                 outcomes=['sucess'],
-                 input_keys=['baxter'],
-                 output_keys=['foo_counter_out'],baxter=None):
-        self.baxter=baxter
-        smach.State.__init__(self,
-                             outcomes=outcomes,
-                             input_keys=['baxter'],
-                             output_keys=['foo_counter_out'])
+	def __init__(self,
+	             outcomes=['sucess'],
+	             input_keys=['baxter'],
+	             output_keys=['foo_counter_out'],
+	             baxter=None):
+		self.baxter = baxter
+		smach.State.__init__(self,
+		                     outcomes=outcomes,
+		                     input_keys=['baxter'],
+		                     output_keys=['foo_counter_out'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state Identify Corner')
+	def execute(self, userdata):
+		rospy.loginfo('Executing state Identify Corner')
 
-        # Initialize position
-        self.baxter.initialize()
-        time.sleep(5)
+		# Initialize position
+		self.baxter.initialize()
+		time.sleep(5)
 
-        time.sleep(2)
-        return 'sucess'
+		time.sleep(2)
+		return 'sucess'
 
 
 # define state Bar
 class GraspCorner(smach.State):
 
-    def __init__(self,baxter=None,corner_id=None):
-        smach.State.__init__(self,
-                             outcomes=['sucess'],
-                             input_keys=['baxter','corner_id'])
-        self.corner_id = corner_id
-        self.baxter = baxter
+	def __init__(self, baxter=None, corner_id=None):
+		smach.State.__init__(self,
+		                     outcomes=['sucess'],
+		                     input_keys=['baxter', 'corner_id'])
+		self.corner_id = corner_id
+		self.baxter = baxter
 
-    def execute(self, userdata):
-        # Verify that the corner is visible
-        rospy.loginfo('Checking Corner Visibility')
-        corner = self.corner_id
-        visible = self.baxter.check_marker_visibility(corner)
+	def execute(self, userdata):
+		# Verify that the corner is visible
+		rospy.loginfo('Checking Corner Visibility')
+		corner = self.corner_id
+		visible = self.baxter.check_marker_visibility(corner)
 
-        while not visible:
-            rospy.loginfo('Corner NOT Visible')
-            rospy.sleep(1)
-            visible = self.baxter.check_marker_visibility(corner)
+		while not visible:
+			rospy.loginfo('Corner NOT Visible')
+			rospy.sleep(1)
+			visible = self.baxter.check_marker_visibility(corner)
 
-        # Grasp corner
-        rospy.loginfo('Executing state Grasp Corner')
-        # rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
+		# Grasp corner
+		rospy.loginfo('Executing state Grasp Corner')
+		# rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
 
-        self.baxter.grasp(corner)  # int
-        time.sleep(5)
+		self.baxter.grasp(corner)  # int
+		time.sleep(5)
 
-        # Close
-        self.baxter.close_gripper()
-        time.sleep(2)
-        return 'sucess'
+		# Close
+		self.baxter.close_gripper()
+		time.sleep(2)
+		return 'sucess'
 
 
 # define state Bar
 class Stretch(smach.State):
 
-    def __init__(self,baxter):
-        smach.State.__init__(self,
-                             outcomes=['sucess'],
-                             input_keys=['baxter'])
-        self.baxter = baxter
+	def __init__(self, baxter):
+		smach.State.__init__(self, outcomes=['sucess'], input_keys=['baxter'])
+		self.baxter = baxter
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state Stretch')
+	def execute(self, userdata):
+		rospy.loginfo('Executing state Stretch')
 
-        # Stretch
-        self.baxter.pull()
-        time.sleep(2)
+		# Stretch
+		self.baxter.pull()
+		time.sleep(2)
 
-        # rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
+		# rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
 
-        return 'sucess'
+		return 'sucess'
 
 
 class Fold(smach.State):
 
-    def __init__(self,baxter,corner_id,edge,closest_corners,final_corners):
-        smach.State.__init__(self,
-                             outcomes=['sucess'],
-                             input_keys=['corner_id','edge','baxter','closest_corners','final_corners'])
+	def __init__(self, baxter, corner_id, edge, closest_corners, final_corners):
+		smach.State.__init__(self,
+		                     outcomes=['sucess'],
+		                     input_keys=[
+		                         'corner_id', 'edge', 'baxter', 'closest_corners',
+		                         'final_corners'
+		                     ])
 
-        self.baxter=baxter
-        self.corner_id=corner_id
-        self.edge=edge
-        self.closest_corners=closest_corners
-        self.final_corners=final_corners
+		self.baxter = baxter
+		self.corner_id = corner_id
+		self.edge = edge
+		self.closest_corners = closest_corners
+		self.final_corners = final_corners
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state Fold')
-        time.sleep(2)
-        # Place
-        corner = self.corner_id
-        edge = self.edge
+	def execute(self, userdata):
+		rospy.loginfo('Executing state Fold')
+		time.sleep(2)
+		# Place
+		corner = self.corner_id
+		edge = self.edge
 
-        self.baxter.place(self.closest_corners[corner], self.final_corners[corner], edge)  # Vector3, Vector3, float
+		self.baxter.place(self.closest_corners[corner], self.final_corners[corner],
+		                  edge)  # Vector3, Vector3, float
 
-        # rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
-        return 'sucess'
+		# rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
+		return 'sucess'
 
 
 class Goal(smach.State):
 
-    def __init__(self, baxter):
-        smach.State.__init__(self,
-                             outcomes=['sucess'],
-                             input_keys=['baxter'])
-        self.baxter = baxter
+	def __init__(self, baxter):
+		smach.State.__init__(self, outcomes=['sucess'], input_keys=['baxter'])
+		self.baxter = baxter
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state GOAL')
-        # Open gripper
-        self.baxter.open_gripper()
-        time.sleep(1)
-        # Added upwards movement
-        self.baxter.move_along_line(np.array([0., 0., 1]), 0.04)
+	def execute(self, userdata):
+		rospy.loginfo('Executing state GOAL')
+		# Open gripper
+		self.baxter.open_gripper()
+		time.sleep(1)
+		# Added upwards movement
+		self.baxter.move_along_line(np.array([0., 0., 1]), 0.04)
 
-        self.baxter.initialize()
-        # rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
-        return 'sucess'
+		self.baxter.initialize()
+		# rospy.loginfo('Counter = %f' % userdata.bar_counter_in)
+		return 'sucess'
 
 
-def sub_state_machine_fold_corner(counter=0, final_flag=False,baxter=None,edge=None,final_corners=None,closest_corners=None,corner_id=None):
-    smach.StateMachine.add(
-        'IdentifyCorner_' + str(counter),
-        IdentifyCorner(baxter=baxter),
-        transitions={'sucess': 'GraspCorner_' + str(counter)})
+def sub_state_machine_fold_corner(counter=0,
+                                  final_flag=False,
+                                  baxter=None,
+                                  edge=None,
+                                  final_corners=None,
+                                  closest_corners=None,
+                                  corner_id=None):
+	smach.StateMachine.add('IdentifyCorner_' + str(counter),
+	                       IdentifyCorner(baxter=baxter),
+	                       transitions={'sucess': 'GraspCorner_' + str(counter)})
 
-    smach.StateMachine.add('GraspCorner_' + str(counter),
-                           GraspCorner(baxter=baxter,corner_id=corner_id),
-                           transitions={
-                               'sucess': 'Stretch_' + str(counter),
-                           })
+	smach.StateMachine.add('GraspCorner_' + str(counter),
+	                       GraspCorner(baxter=baxter, corner_id=corner_id),
+	                       transitions={
+	                           'sucess': 'Stretch_' + str(counter),
+	                       })
 
-    smach.StateMachine.add('Stretch_' + str(counter),
-                           Stretch(baxter=baxter),
-                           transitions={
-                               'sucess': 'Fold_' + str(counter),
-                           })
+	smach.StateMachine.add('Stretch_' + str(counter),
+	                       Stretch(baxter=baxter),
+	                       transitions={
+	                           'sucess': 'Fold_' + str(counter),
+	                       })
 
-    smach.StateMachine.add('Fold_' + str(counter),
-                           Fold(baxter, corner_id, edge, closest_corners, final_corners),
-                           transitions={'sucess': 'Goal_' + str(counter)})
+	smach.StateMachine.add('Fold_' + str(counter),
+	                       Fold(baxter, corner_id, edge, closest_corners,
+	                            final_corners),
+	                       transitions={'sucess': 'Goal_' + str(counter)})
 
-    if not final_flag:
-        smach.StateMachine.add(
-            'Goal_' + str(counter),
-            Goal(baxter),
-            transitions={'sucess': 'IdentifyCorner_' + str(counter + 1)})
-    else:
-        smach.StateMachine.add('Goal_' + str(counter),
-                               Goal(baxter),
-                               transitions={'sucess': 'folded'})
+	if not final_flag:
+		smach.StateMachine.add(
+		    'Goal_' + str(counter),
+		    Goal(baxter),
+		    transitions={'sucess': 'IdentifyCorner_' + str(counter + 1)})
+	else:
+		smach.StateMachine.add('Goal_' + str(counter),
+		                       Goal(baxter),
+		                       transitions={'sucess': 'folded'})
 
 
 def main():
-    rospy.init_node('folding_state_machine')
+	rospy.init_node('folding_state_machine')
 
-    # remap again
-    joint_state_topic = ['joint_states:=/robot/joint_states']
-    moveit_commander.roscpp_initialize(joint_state_topic)
-    time.sleep(5)
-    # init class obj
-    baxter = BaxterWrapping()
-    time.sleep(5)
-    ############### INITIALIZATION ##############
-    # Initialize right and left arm
-    baxter.rabp_go_base_pose()
-    baxter.labp_go_base_pose()
-    baxter.open_left_gripper()
-    baxter.open_right_gripper()
+	# remap again
+	joint_state_topic = ['joint_states:=/robot/joint_states']
+	moveit_commander.roscpp_initialize(joint_state_topic)
+	time.sleep(5)
+	# init class obj
+	baxter = BaxterWrapping()
+	time.sleep(5)
+	############### INITIALIZATION ##############
+	# Initialize right and left arm
+	baxter.rabp_go_base_pose()
+	baxter.labp_go_base_pose()
+	baxter.open_left_gripper()
+	baxter.open_right_gripper()
 
-    # Get Box estimations from vision
-    A = Vector3()
-    A.x = 0.825
-    A.y = -0.05
-    A.z = -0.14
+	# Get Box estimations from vision
+	A = Vector3()
+	A.x = 0.825
+	A.y = -0.05
+	A.z = -0.14
 
-    B = Vector3()
-    B.x = 0.73
-    B.y = 0.11
-    B.z = -0.14
+	B = Vector3()
+	B.x = 0.73
+	B.y = 0.11
+	B.z = -0.14
 
-    C = Vector3()
-    C.x = 0.58
-    C.y = 0.
-    C.z = -0.14
+	C = Vector3()
+	C.x = 0.58
+	C.y = 0.
+	C.z = -0.14
 
-    D = Vector3()
-    D.x = 0.72
-    D.y = -0.17
-    D.z = -0.14
+	D = Vector3()
+	D.x = 0.72
+	D.y = -0.17
+	D.z = -0.14
 
-    closest_corners = [A, B, C, D]
-    final_corners = [C, D, A, B]
-    edge = 0.08
+	closest_corners = [A, B, C, D]
+	final_corners = [C, D, A, B]
+	edge = 0.08
 
-    # Create a SMACH state machine
-    sm_top = smach.StateMachine(outcomes=['final_outcome'])
-    sm_top.userdata.sm_counter = 0
-    sm_top.userdata.corner_id = 0
-    sm_top.userdata.baxter = baxter
-    sm_top.userdata.final_corners = final_corners
-    sm_top.userdata.closest_corners = closest_corners
-    sm_top.userdata.edge = edge
+	# Create a SMACH state machine
+	sm_top = smach.StateMachine(outcomes=['final_outcome'])
+	sm_top.userdata.sm_counter = 0
+	sm_top.userdata.corner_id = 0
+	sm_top.userdata.baxter = baxter
+	sm_top.userdata.final_corners = final_corners
+	sm_top.userdata.closest_corners = closest_corners
+	sm_top.userdata.edge = edge
 
-    # sm.userdata.corner_positon
+	# sm.userdata.corner_positon
 
-    # Open the container
-    counter_fold = 0
-    max_goal_counter = 3
-    with sm_top:
-        # Add states to the container
+	# Open the container
+	counter_fold = 0
+	max_goal_counter = 3
+	with sm_top:
+		# Add states to the container
 
-        smach.StateMachine.add('Start',
-                               Start(),
-                               transitions={'sucess': 'FinalGoal'})
+		smach.StateMachine.add('Start',
+		                       Start(),
+		                       transitions={'sucess': 'FinalGoal'})
 
-        sm_sub = smach.StateMachine(outcomes=['folded'])
-        sm_sub.userdata.corner_id = 0
-        sm_sub.userdata.edge = edge
+		sm_sub = smach.StateMachine(outcomes=['folded'])
+		sm_sub.userdata.corner_id = 0
+		sm_sub.userdata.edge = edge
 
-        with sm_sub:
-            sm_sub.userdata.baxter = baxter
-            sm_sub.userdata.final_corners = final_corners
-            sm_sub.userdata.closest_corners = closest_corners
-            sm_sub.userdata.edge = edge
-            sm_sub.userdata.corner_id = 0
-            sub_state_machine_fold_corner(counter=0,baxter=baxter,edge=edge,final_corners=final_corners,corner_id=0,closest_corners=closest_corners)
-            sm_sub.userdata.corner_id = 1
-            sub_state_machine_fold_corner(counter=1,baxter=baxter,edge=edge,final_corners=final_corners,corner_id=1,closest_corners=closest_corners)
-            sm_sub.userdata.corner_id = 2
-            sub_state_machine_fold_corner(counter=2,baxter=baxter,edge=edge,final_corners=final_corners,corner_id=2,closest_corners=closest_corners)
-            sm_sub.userdata.corner_id = 3
-            sub_state_machine_fold_corner(counter=3, final_flag=True,baxter=baxter,edge=edge,final_corners=final_corners,corner_id=3,closest_corners=closest_corners)
+		with sm_sub:
+			sm_sub.userdata.baxter = baxter
+			sm_sub.userdata.final_corners = final_corners
+			sm_sub.userdata.closest_corners = closest_corners
+			sm_sub.userdata.edge = edge
+			sm_sub.userdata.corner_id = 0
+			sub_state_machine_fold_corner(counter=0,
+			                              baxter=baxter,
+			                              edge=edge,
+			                              final_corners=final_corners,
+			                              corner_id=0,
+			                              closest_corners=closest_corners)
+			sm_sub.userdata.corner_id = 1
+			sub_state_machine_fold_corner(counter=1,
+			                              baxter=baxter,
+			                              edge=edge,
+			                              final_corners=final_corners,
+			                              corner_id=1,
+			                              closest_corners=closest_corners)
+			sm_sub.userdata.corner_id = 2
+			sub_state_machine_fold_corner(counter=2,
+			                              baxter=baxter,
+			                              edge=edge,
+			                              final_corners=final_corners,
+			                              corner_id=2,
+			                              closest_corners=closest_corners)
+			sm_sub.userdata.corner_id = 3
+			sub_state_machine_fold_corner(counter=3,
+			                              final_flag=True,
+			                              baxter=baxter,
+			                              edge=edge,
+			                              final_corners=final_corners,
+			                              corner_id=3,
+			                              closest_corners=closest_corners)
 
-        smach.StateMachine.add('FinalGoal',
-                               sm_sub,
-                               transitions={'folded': 'final_outcome'})
+		smach.StateMachine.add('FinalGoal',
+		                       sm_sub,
+		                       transitions={'folded': 'final_outcome'})
 
-    # Create and start the introspection server
-    #sis = smach_ros.IntrospectionServer('server_name', sm_top, '/SM_ROOT')
-    #sis.start()
+	# Create and start the introspection server
+	#sis = smach_ros.IntrospectionServer('server_name', sm_top, '/SM_ROOT')
+	#sis.start()
 
-    # Execute SMACH plan
-    outcome = sm_top.execute()
+	# Execute SMACH plan
+	outcome = sm_top.execute()
 
-    # Wait for ctrl-c to stop the application
-    rospy.spin()
-    #sis.stop()
+	# Wait for ctrl-c to stop the application
+	rospy.spin()
+	#sis.stop()
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- src/state_machine/utils.py
+++ src/state_machine/utils.py
@@ -3,37 +3,39 @@
 import time
 import copy as copy_module
 
-
-
 ################ Util functions #####################
 
+
 def get_pose_stamped_str(ps):
-    return "["+str(ps.pose.position.x)+", "+str(ps.pose.position.y)+", "+str(ps.pose.position.z)+"]"
+	return "[" + str(ps.pose.position.x) + ", " + str(
+	    ps.pose.position.y) + ", " + str(ps.pose.position.z) + "]"
+
 
 def copy_pose_stamped(msg_in):
-    return copy_module.deepcopy(msg_in)
-    
-def plan_group(group, dest_pose, sleep_time, printed_str = ""): 
-    
+	return copy_module.deepcopy(msg_in)
 
-    curr_pose = group.get_current_pose()
-    print(printed_str+" ---- Goal:"+get_pose_stamped_str(dest_pose)+", From:" + get_pose_stamped_str(curr_pose))
-    group.set_start_state_to_current_state()
-    group.set_pose_target(dest_pose)
-    success = group.go(wait=True)
 
-    group.stop()
-    time.sleep(sleep_time)
-    return success
+def plan_group(group, dest_pose, sleep_time, printed_str=""):
 
-def plan_cartesian_group(group, waypoint_list, sleep_time, printed_str = ""):
-    (plan, fraction) = group.compute_cartesian_path(
-                               waypoint_list,  0.01, 0.0) 
-    success = group.execute(plan, wait=True)
+	curr_pose = group.get_current_pose()
+	print(printed_str + " ---- Goal:" + get_pose_stamped_str(dest_pose) +
+	      ", From:" + get_pose_stamped_str(curr_pose))
+	group.set_start_state_to_current_state()
+	group.set_pose_target(dest_pose)
+	success = group.go(wait=True)
 
-    group.stop()
-    time.sleep(sleep_time)
-    return success
+	group.stop()
+	time.sleep(sleep_time)
+	return success
+
+
+def plan_cartesian_group(group, waypoint_list, sleep_time, printed_str=""):
+	(plan, fraction) = group.compute_cartesian_path(waypoint_list, 0.01, 0.0)
+	success = group.execute(plan, wait=True)
+
+	group.stop()
+	time.sleep(sleep_time)
+	return success
+
 
 ################ End util functions #####################
-
